2ef936051433b0954eda0741f45753406dcd4951
JayO <45680293+ojhajayant@users.noreply.github.com>
2020-03-08 09:31:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup
 
+setup(
+    name='eva',
+    version='0.0.1',
+    description='My private package from private EVA repo',https://github.com/ojhajayant/eva https://github.com/ojhajayant/eva.git
+    url='git@github.com:ojhajayant/eva.git',
+    author='Jayant Ojha',
+    author_email='ojhajayant@yahoo.com',
+    license='unlicense',
+    packages=['week7'],
+    zip_safe=False
+)
