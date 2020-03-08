c79284940016609d8024be05ffff30861417dadf
JayO <45680293+ojhajayant@users.noreply.github.com>
2020-03-08 12:46:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup#,find_packages
+from setuptools import setup, find_packages
 
 setup(
-    #packages=find_packages(),
     name='eva',
     version='0.0.9',
     description='My private package from private EVA repo',
-    url='git@github.com:ojhajayant/eva.git',
+    url='https://github.com/ojhajayant/eva',
     author='Jayant Ojha',
     author_email='ojhajayant@yahoo.com',
     license='unlicense',
-    packages=['week7', 'week7.modular'],
+    package_dir={'': 'eva'}, 
+    packages=find_packages(where='eva'),
+    #packages=['week7', 'week7.modular'],
     zip_safe=False
 )
