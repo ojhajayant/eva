37a5abf79207fbdf253e10cc26fee79f90378455
JayO <45680293+ojhajayant@users.noreply.github.com>
2020-03-08 10:21:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup#,find_packages
+from setuptools import setup,find_packages
 
 setup(
-    #packages=find_packages(),
+    packages=find_packages(),
     name='eva',
     version='0.0.1',
     description='My private package from private EVA repo',
@@ -9,6 +9,6 @@ setup(
     author='Jayant Ojha',
     author_email='ojhajayant@yahoo.com',
     license='unlicense',
-    packages=['week7', 'week7.modular'],
+    #packages=['week7', 'week7.modular'],
     zip_safe=False
 )
