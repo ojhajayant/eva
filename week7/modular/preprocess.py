a041a8bbe13543bbff9a511526f155113ce6bd2c
JayO <45680293+ojhajayant@users.noreply.github.com>
2020-03-08 14:59:37 +0530

    Update preprocess.py

--- week7/modular/preprocess.py
+++ week7/modular/preprocess.py
@@ -15,8 +15,8 @@ import matplotlib.pyplot as plt
 import os
 
 sys.path.append('./')
-args, unknown = cfg.parser.parse_known_args()
-#args = cfg.parser.parse_args()
+#args, unknown = cfg.parser.parse_known_args() #parser.parse_args(args=[])
+args = cfg.parser.parse_args(args=[])
 file_path = args.data
 
 
