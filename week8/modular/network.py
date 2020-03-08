ab32803257712676160734261a1ffcf437884d88
JayO <45680293+ojhajayant@users.noreply.github.com>
2020-03-08 21:46:23 +0530

    Update network.py

--- week8/modular/network.py
+++ week8/modular/network.py
@@ -71,8 +71,8 @@ class ResNet(nn.Module):
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
+        out = self.linear(out) 
+        return F.log_softmax(out, dim=-1)
 
 
 def ResNet18():
