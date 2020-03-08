55e5013ec18bdc5793e0294984ab6e313bc7c427
JayO <45680293+ojhajayant@users.noreply.github.com>
2020-03-08 12:29:26 +0530

    Update utils.py

--- week7/modular/utils.py
+++ week7/modular/utils.py
@@ -7,14 +7,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import shutil
-import week7.modular.cfg as cfg
+import week7.modular.cfg
 import torch
 import os
 import json
 from sklearn.metrics import confusion_matrix, classification_report
 
 sys.path.append('./')
-args = cfg.parser.parse_args()
+args = week7.modular.cfg.parser.parse_args()
 file_path = args.data
 
 
