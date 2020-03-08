0cb64041455671c2e70e02d9cde19d774f95362f
JayO <45680293+ojhajayant@users.noreply.github.com>
2020-03-08 13:24:12 +0530

    Update __init__.py

--- week7/modular/__init__.py
+++ week7/modular/__init__.py
@@ -1,6 +1,6 @@
-#import week7.modular.cfg
-#import week7.modular.utils
-#import week7.modular.main
-#import week7.modular.preprocess
-#import week7.modular.train
-#import week7.modular.test
+from modular import cfg
+from modular import utils
+from modular import main
+from modular import preprocess
+from modular import train
+from modular import test
