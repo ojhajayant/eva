7b3467bba21bdf468f508f5a7066895f8ae1f176
JayO <45680293+ojhajayant@users.noreply.github.com>
2020-03-08 22:31:35 +0530

    Update preprocess.py

--- week8/modular/preprocess.py
+++ week8/modular/preprocess.py
@@ -19,6 +19,41 @@ args = cfg.parser.parse_args(args=[])
 file_path = args.data
 
 
+def cutout(mask_size, p, cutout_inside, mask_color=(0, 0, 0)):
+    mask_size_half = mask_size // 2
+    offset = 1 if mask_size % 2 == 0 else 0
+
+    def _cutout(image):
+        image = np.asarray(image).copy()
+
+        if np.random.random() > p:
+            return image
+
+        h, w = image.shape[:2]
+
+        if cutout_inside:
+            cxmin, cxmax = mask_size_half, w + offset - mask_size_half
+            cymin, cymax = mask_size_half, h + offset - mask_size_half
+        else:
+            cxmin, cxmax = 0, w + offset
+            cymin, cymax = 0, h + offset
+
+        cx = np.random.randint(cxmin, cxmax)
+        cy = np.random.randint(cymin, cymax)
+        xmin = cx - mask_size_half
+        ymin = cy - mask_size_half
+        xmax = xmin + mask_size
+        ymax = ymin + mask_size
+        xmin = max(0, xmin)
+        ymin = max(0, ymin)
+        xmax = min(w, xmax)
+        ymax = min(h, ymax)
+        image[ymin:ymax, xmin:xmax] = mask_color
+        return image
+
+    return _cutout
+
+
 def get_dataset_mean_std():
     """
     Get the CIFAR10 dataset mean and std to be used as tuples
@@ -47,10 +82,16 @@ def preprocess_data(mean_tuple, std_tuple):
         #  transforms.Resize((28, 28)),
         #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
         # transforms.RandomRotation((-10.0, 10.0), fill=(1,)),
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        cutout(config['cutout_size'],
+               config['cutout_prob'],
+               config['cutout_inside'])
         transforms.ToTensor(),
         transforms.Normalize(mean_tuple, std_tuple)
         # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values.
         # Note the difference between (0.1307) and (0.1307,)
+        
     ])
 
     # Test Phase transformations
