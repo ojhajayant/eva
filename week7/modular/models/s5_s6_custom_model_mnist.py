38bcbb5cf8665ba9585caf74c0994cad06e10e2f
JayO <45680293+ojhajayant@users.noreply.github.com>
2020-03-15 16:59:03 +0530

    Add files via upload

---
+++ week7/modular/models/s5_s6_custom_model_mnist.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+"""
+s6_custom_model_mnist.py: This contains the model definition used in session6
+to be trained on MNIST dataset.
+"""
+from __future__ import print_function
+
+import sys
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+from week7.modular import cfg
+
+sys.path.append('./')
+
+args = cfg.parser.parse_args(args=[])
+dropout_value = args.dropout
+
+dropout_value = 0.1  # Default for this network
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # Input Block
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), padding=0, bias=False),
+            nn.BatchNorm2d(8),
+            nn.Dropout(dropout_value),
+            nn.ReLU(),
+        )  # input:28x28x1, output:26x26x8, RF:3x3
+
+        # CONVOLUTION BLOCK 1
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=8, kernel_size=(3, 3), padding=0, bias=False),
+            nn.BatchNorm2d(8),
+            nn.Dropout(dropout_value),
+            nn.ReLU(),
+        )  # input:26x26x8, output:24x24x8, RF:5x5
+
+        # TRANSITION BLOCK 1
+        self.pool1 = nn.MaxPool2d(2, 2)  # input:24x24x8, output:12x12x8, RF:6x6
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=8, kernel_size=(1, 1), padding=0, bias=False),
+            nn.BatchNorm2d(8),
+            nn.Dropout(dropout_value),
+            nn.ReLU(),
+        )  # input:12x12x8, output:12x12x8, RF:6x6
+
+        # CONVOLUTION BLOCK 2
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value),
+            nn.ReLU(),
+        )  # input:12x12x8, output:10x10x16, RF:10x10
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value),
+            nn.ReLU(),
+        )  # input:10x10x16, output:8x8x16, RF:14x14
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value),
+            nn.ReLU(),
+        )  # input:8x8x16, output:6x6x16, RF:18x18
+        self.convblock7 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=20, kernel_size=(3, 3), padding=1, bias=False),
+            nn.BatchNorm2d(20),
+            nn.Dropout(dropout_value),
+            nn.ReLU(),
+        )  # input:6x6x16, output:6x6x16, RF:22x22
+
+        # OUTPUT BLOCK
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=6)
+        )  # input:6x6x16, output:1x1x16, RF:32x32
+
+        self.convblock8 = nn.Sequential(
+            nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+        )  # input:1x1x16, output:1x1x10,
+
+    def forward(self, x):
+        # Input Block
+        x = self.convblock1(x)
+        # CONVOLUTION BLOCK 1
+        x = self.convblock2(x)
+        # TRANSITION BLOCK 1
+        x = self.pool1(x)
+        x = self.convblock3(x)
+        # CONVOLUTION BLOCK 2
+        x = self.convblock4(x)
+        x = self.convblock5(x)
+        x = self.convblock6(x)
+        x = self.convblock7(x)
+        # OUTPUT BLOCK
+        x = self.gap(x)
+        x = self.convblock8(x)
+        # Reshape
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=-1)
