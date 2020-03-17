00c66b24619909ac52ab3caf1fcd7c73e034b30d
JayO <45680293+ojhajayant@users.noreply.github.com>
2020-03-17 14:05:33 +0530

    Add files via upload

---
+++ week7/modular/grad_cam.py
@@ -0,0 +1,361 @@
+import torch
+import torch.nn.functional as F
+
+import cv2
+import numpy as np
+import torch
+
+import os
+import sys
+import warnings
+
+import numpy as np
+import torch
+import torch.optim as optim
+from torchsummary import summary
+
+from week7.modular import cfg
+from week7.modular.models import resnet18, s5_s6_custom_model_mnist, s7_custom_model_cifar10, QuizDNN
+from week7.modular import preprocess
+from week7.modular import preprocess_albumentations
+from week7.modular import test
+from week7.modular import train
+from week7.modular import utils
+
+sys.path.append('./')
+# args = cfg.parser.parse_args(args=[])
+global args
+args = cfg.args
+if args.cmd == None:
+    args.cmd = 'train'
+
+import sys
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+from week7.modular import cfg
+
+sys.path.append('./')
+
+
+def visualize_cam(mask, img):
+    """Make heatmap from mask and synthesize GradCAM result image using heatmap and img.
+    Args:
+        mask (torch.tensor): mask shape of (1, 1, H, W) and each element has value in range [0, 1]
+        img (torch.tensor): img shape of (1, 3, H, W) and each pixel value is in range [0, 1]
+
+    Return:
+        heatmap (torch.tensor): heatmap img shape of (3, H, W)
+        result (torch.tensor): synthesized GradCAM result of same shape with heatmap.
+    """
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask.cpu().squeeze()), cv2.COLORMAP_JET)
+    heatmap = torch.from_numpy(heatmap).permute(2, 0, 1).float().div(255)
+    b, g, r = heatmap.split(1)
+    heatmap = torch.cat([r, g, b])
+
+    result = heatmap + img.cpu()
+    result = result.div(result.max()).squeeze()
+
+    return heatmap, result
+
+
+def find_resnet_layer(arch, target_layer_name):
+    """Find resnet layer to calculate GradCAM and GradCAM++
+
+    Args:
+        arch: default torchvision densenet models
+        target_layer_name (str): the name of layer with its hierarchical information. please refer to usages below.
+            target_layer_name = 'conv1'
+            target_layer_name = 'layer1'
+            target_layer_name = 'layer1_basicblock0'
+            target_layer_name = 'layer1_basicblock0_relu'
+            target_layer_name = 'layer1_bottleneck0'
+            target_layer_name = 'layer1_bottleneck0_conv1'
+            target_layer_name = 'layer1_bottleneck0_downsample'
+            target_layer_name = 'layer1_bottleneck0_downsample_0'
+            target_layer_name = 'avgpool'
+            target_layer_name = 'fc'
+
+    Return:
+        target_layer: found layer. this layer will be hooked to get forward/backward pass information.
+    """
+    if 'layer' in target_layer_name:
+        hierarchy = target_layer_name.split('_')
+        layer_num = int(hierarchy[0].lstrip('layer'))
+        if layer_num == 1:
+            target_layer = arch.layer1
+        elif layer_num == 2:
+            target_layer = arch.layer2
+        elif layer_num == 3:
+            target_layer = arch.layer3
+        elif layer_num == 4:
+            target_layer = arch.layer4
+        else:
+            raise ValueError('unknown layer : {}'.format(target_layer_name))
+
+        if len(hierarchy) >= 2:
+            bottleneck_num = int(hierarchy[1].lower().lstrip('bottleneck').lstrip('basicblock'))
+            target_layer = target_layer[bottleneck_num]
+
+        if len(hierarchy) >= 3:
+            target_layer = target_layer._modules[hierarchy[2]]
+
+        if len(hierarchy) == 4:
+            target_layer = target_layer._modules[hierarchy[3]]
+
+    else:
+        target_layer = arch._modules[target_layer_name]
+
+    return target_layer
+
+
+def denormalize(tensor, mean, std):
+    if not tensor.ndimension() == 4:
+        raise TypeError('tensor should be 4D')
+
+    mean = torch.FloatTensor(mean).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
+    std = torch.FloatTensor(std).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
+
+    return tensor.mul(std).add(mean)
+
+
+def normalize(tensor, mean, std):
+    if not tensor.ndimension() == 4:
+        raise TypeError('tensor should be 4D')
+
+    mean = torch.FloatTensor(mean).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
+    std = torch.FloatTensor(std).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
+
+    return tensor.sub(mean).div(std)
+
+
+class Normalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        return self.do(tensor)
+
+    def do(self, tensor):
+        return normalize(tensor, self.mean, self.std)
+
+    def undo(self, tensor):
+        return denormalize(tensor, self.mean, self.std)
+
+
+class GradCAM(object):
+    """Calculate GradCAM salinecy map.
+    A simple example:
+        # initialize a model, model_dict and gradcam
+        resnet = torchvision.models.resnet101(pretrained=True)
+        resnet.eval()
+        model_dict = dict(model_type='resnet', arch=resnet, layer_name='layer4', input_size=(32, 32))
+        gradcam = GradCAM(model_dict)
+        # get an image and normalize with mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
+        img = load_img()
+        normed_img = normalizer(img)
+        # get a GradCAM saliency map on the class index 10.
+        mask, logit = gradcam(normed_img, class_idx=10)
+        # make heatmap from mask and synthesize saliency map using heatmap and img
+        heatmap, cam_result = visualize_cam(mask, img)
+    Args:
+        model_dict (dict): a dictionary that contains 'model_type', 'arch', layer_name', 'input_size'(optional) as keys.
+        verbose (bool): whether to print output size of the saliency map givien 'layer_name' and 'input_size' in model_dict.
+    """
+
+    def __init__(self, model_dict, verbose=False):
+        model_type = model_dict['type']
+        layer_name = model_dict['layer_name']
+        self.model_arch = model_dict['arch']
+
+        self.gradients = dict()
+        self.activations = dict()
+
+        def backward_hook(module, grad_input, grad_output):
+            self.gradients['value'] = grad_output[0]
+            return None
+
+        def forward_hook(module, input, output):
+            self.activations['value'] = output
+            return None
+
+        if 'resnet' in model_type.lower():
+            target_layer = find_resnet_layer(self.model_arch, layer_name)
+
+        target_layer.register_forward_hook(forward_hook)
+        target_layer.register_backward_hook(backward_hook)
+
+        if verbose:
+            try:
+                input_size = model_dict['input_size']
+            except KeyError:
+                print("please specify size of input image in model_dict. e.g. {'input_size':(32, 32)}")
+                pass
+            else:
+                device = 'cuda' if next(self.model_arch.parameters()).is_cuda else 'cpu'
+                self.model_arch(torch.zeros(1, 3, *(input_size), device=device))
+                print('saliency_map size :', self.activations['value'].shape[2:])
+
+    def forward(self, input, class_idx=None, retain_graph=False):
+        """
+        Args:
+            input: input image with shape of (1, 3, H, W)
+            class_idx (int): class index for calculating GradCAM.
+                    If not specified, the class index that makes the highest model prediction score will be used.
+        Return:
+            mask: saliency map of the same spatial dimension with input
+            logit: model output
+        """
+        b, c, h, w = input.size()
+
+        logit = self.model_arch(input)
+        if class_idx is None:
+            score = logit[:, logit.max(1)[-1]].squeeze()
+        else:
+            score = logit[:, class_idx].squeeze()
+
+        self.model_arch.zero_grad()
+        score.backward(retain_graph=retain_graph)
+        gradients = self.gradients['value']
+        activations = self.activations['value']
+        b, k, u, v = gradients.size()
+
+        alpha = gradients.view(b, k, -1).mean(2)
+        # alpha = F.relu(gradients.view(b, k, -1)).mean(2)
+        weights = alpha.view(b, k, 1, 1)
+
+        saliency_map = (weights * activations).sum(1, keepdim=True)
+        saliency_map = F.relu(saliency_map)
+        saliency_map = F.upsample(saliency_map, size=(h, w), mode='bilinear', align_corners=False)
+        saliency_map_min, saliency_map_max = saliency_map.min(), saliency_map.max()
+        saliency_map = (saliency_map - saliency_map_min).div(saliency_map_max - saliency_map_min).data
+
+        return saliency_map, logit
+
+    def __call__(self, input, class_idx=None, retain_graph=False):
+        return self.forward(input, class_idx, retain_graph)
+
+
+class GradCAMpp(GradCAM):
+    """Calculate GradCAM++ salinecy map.
+    A simple example:
+        # initialize a model, model_dict and gradcampp
+        resnet = torchvision.models.resnet101(pretrained=True)
+        resnet.eval()
+        model_dict = dict(model_type='resnet', arch=resnet, layer_name='layer4', input_size=(32, 32))
+        gradcampp = GradCAMpp(model_dict)
+        # get an image and normalize with mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
+        img = load_img()
+        normed_img = normalizer(img)
+        # get a GradCAM saliency map on the class index 10.
+        mask, logit = gradcampp(normed_img, class_idx=10)
+        # make heatmap from mask and synthesize saliency map using heatmap and img
+        heatmap, cam_result = visualize_cam(mask, img)
+    Args:
+        model_dict (dict): a dictionary that contains 'model_type', 'arch', layer_name', 'input_size'(optional) as keys.
+        verbose (bool): whether to print output size of the saliency map givien 'layer_name' and 'input_size' in model_dict.
+    """
+
+    def __init__(self, model_dict, verbose=False):
+        super(GradCAMpp, self).__init__(model_dict, verbose)
+
+    def forward(self, input, class_idx=None, retain_graph=False):
+        """
+        Args:
+            input: input image with shape of (1, 3, H, W)
+            class_idx (int): class index for calculating GradCAM.
+                    If not specified, the class index that makes the highest model prediction score will be used.
+        Return:
+            mask: saliency map of the same spatial dimension with input
+            logit: model output
+        """
+        b, c, h, w = input.size()
+
+        logit = self.model_arch(input)
+        if class_idx is None:
+            score = logit[:, logit.max(1)[-1]].squeeze()
+        else:
+            score = logit[:, class_idx].squeeze()
+
+        self.model_arch.zero_grad()
+        score.backward(retain_graph=retain_graph)
+        gradients = self.gradients['value']  # dS/dA
+        activations = self.activations['value']  # A
+        b, k, u, v = gradients.size()
+
+        alpha_num = gradients.pow(2)
+        alpha_denom = gradients.pow(2).mul(2) + \
+                      activations.mul(gradients.pow(3)).view(b, k, u * v).sum(-1, keepdim=True).view(b, k, 1, 1)
+        alpha_denom = torch.where(alpha_denom != 0.0, alpha_denom, torch.ones_like(alpha_denom))
+
+        alpha = alpha_num.div(alpha_denom + 1e-7)
+        positive_gradients = F.relu(score.exp() * gradients)  # ReLU(dY/dA) == ReLU(exp(S)*dS/dA))
+        weights = (alpha * positive_gradients).view(b, k, u * v).sum(-1).view(b, k, 1, 1)
+
+        saliency_map = (weights * activations).sum(1, keepdim=True)
+        saliency_map = F.relu(saliency_map)
+        saliency_map = F.upsample(saliency_map, size=(32, 32), mode='bilinear', align_corners=False)
+        saliency_map_min, saliency_map_max = saliency_map.min(), saliency_map.max()
+        saliency_map = (saliency_map - saliency_map_min).div(saliency_map_max - saliency_map_min).data
+
+        return saliency_map, logit
+
+
+if __name__ == '__main__':
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+    import os
+    import PIL
+    import numpy as np
+    import torch
+    import torch.nn.functional as F
+    import torchvision.models as models
+    from torchvision.utils import make_grid, save_image
+
+    #Load image
+    img_dir = './data'
+    img_name = 'water-bird.JPEG'
+    img_path = os.path.join(img_dir, img_name)
+    pil_img = PIL.Image.open(img_path)
+    pil_img
+
+    #preprocess image
+    normalizer = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    torch_img = torch.from_numpy(np.asarray(pil_img)).permute(2, 0, 1).unsqueeze(0).float().div(255).cuda()
+    torch_img = F.upsample(torch_img, size=(32, 32), mode='bilinear', align_corners=False)
+    normed_torch_img = normalizer(torch_img)
+
+    #Load torchvision models and make model dictionaries
+    device = torch.device("cuda" if args.cuda else "cpu")
+    model_name = 'CIFAR10_model_epoch-30_L1-1_L2-0_val_acc-88.03.h5'
+    print("Loaded the best model: {} from last training session".format(model_name))
+    resnet = utils.load_model(resnet18.ResNet18(), device, model_name=model_name)
+    resnet.eval(), resnet.cuda();
+    cam_dict = dict()
+
+    resnet_model_dict = dict(type='resnet', arch=resnet, layer_name='layer2', input_size=(32, 32))
+    resnet_gradcam = GradCAM(resnet_model_dict, True)
+    resnet_gradcampp = GradCAMpp(resnet_model_dict, True)
+    cam_dict['resnet'] = [resnet_gradcam, resnet_gradcampp]
+
+    #Feedforward image, calculate GradCAM/GradCAM++, and gather results
+    images = []
+    for gradcam, gradcam_pp in cam_dict.values():
+        mask, _ = gradcam(normed_torch_img)
+        heatmap, result = visualize_cam(mask, torch_img)
+        mask_pp, _ = gradcam_pp(normed_torch_img)
+        heatmap_pp, result_pp = visualize_cam(mask_pp, torch_img)
+        images.append(torch.stack([torch_img.squeeze().cpu(), heatmap, heatmap_pp, result, result_pp], 0))
+    images = make_grid(torch.cat(images, 0), nrow=5)
+
+    #Save and show results
+    output_dir = './data'
+    os.makedirs(output_dir, exist_ok=True)
+    output_name = img_name
+    output_path = os.path.join(output_dir, output_name)
+    save_image(images, output_path)
+    PIL.Image.open(output_path)
+
+
--- week7/modular/utils.py
+++ week7/modular/utils.py
@@ -9,6 +9,13 @@ import os
 import shutil
 import sys
 
+import os
+import PIL
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torchvision.models as models
+from torchvision.utils import make_grid, save_image
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -21,6 +28,8 @@ sys.path.append('./')
 global args
 args = cfg.args
 file_path = args.data
+
+
 # IPYNB_ENV = True  # By default ipynb notebook env
 
 
@@ -131,6 +140,73 @@ def display_mislabelled(model, device, x_test, y_test, y_pred, test_dataset, tit
                 plt.savefig(filepath)
     if args.IPYNB_ENV:
         plt.show()
+
+
+
+import os
+import PIL
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torchvision.models as models
+from torchvision.utils import make_grid, save_image
+
+
+
+def orig_img(norm_img, mean_tuple, std_tuple):
+    for i in range(norm_img.shape[0]):
+        norm_img[i] = (norm_img[i] * std_tuple[i]) + mean_tuple[i]
+    return np.transpose(norm_img, (1, 2, 0))
+
+
+def show_gradcam(model, device, x_test, y_test, y_pred, test_dataset, mean_tuple, std_tuple, class_names, title_str, layer_name="block5_pool",
+                 disp_nums=5, fig_size=(40, 40), size=(32, 32), model_type='resnet', layer='layer3'):
+    import matplotlib.gridspec as gridspec
+    NUM_DISP = disp_nums
+    for _ in range(NUM_DISP):
+        fig = plt.figure(figsize=fig_size)
+        outer = gridspec.GridSpec(1, len(class_names), wspace=0.2, hspace=0.2)
+        for i in range(len(class_names)):
+            inner = gridspec.GridSpecFromSubplotSpec(2, 1,
+                                                     subplot_spec=outer[i], wspace=0.1, hspace=0.1)
+            idx = np.where(y_test[:] == i)[0]
+            features_idx = x_test[idx, ::]
+            img_num = np.random.randint(features_idx.shape[0])
+            im_norm = features_idx[img_num]
+            im_orig = orig_img(im_norm, mean_tuple, std_tuple)
+            normalizer = Normalize(mean=list(mean_tuple), std=list(std_tuple))#Pick this routine from grad_cam module
+            torch_img = torch.from_numpy(np.asarray(im_orig)).permute(2, 0, 1).unsqueeze(0).float().div(255).cuda()
+            torch_img = F.upsample(torch_img, size=size, mode='bilinear', align_corners=False)
+            normed_torch_img = normalizer(torch_img)
+
+            model.eval(), model.cuda();
+            cam_dict = dict()
+
+            model_dict = dict(type=model_type, arch=model, layer_name=layer, input_size=size)
+            model_gradcam = GradCAM(model_dict, True)
+            model_gradcampp = GradCAMpp(model_dict, True)
+            cam_dict[model_type] = [model_gradcam, model_gradcampp]
+
+            # Feedforward image, calculate GradCAM/GradCAM++, and gather results
+            images = []
+            for gradcam, gradcam_pp in cam_dict.values():
+                mask, _ = gradcam(normed_torch_img)
+                heatmap, result = visualize_cam(mask, torch_img)
+                mask_pp, _ = gradcam_pp(normed_torch_img)
+                heatmap_pp, result_pp = visualize_cam(mask_pp, torch_img)
+            for j in range(2):
+                ax = plt.Subplot(fig, inner[j])
+                if args.dataset == 'CIFAR10':
+                    _ = ax.set_title('Act:{} '.format(test_dataset.classes[int(i)]) + ' Pred:{} '.format(
+                        test_dataset.classes[int(y_pred[idx[img_num]][0])]), fontsize=24)
+                elif args.dataset == 'MNIST':
+                    _ = ax.set_title('Act:{} '.format(i) + ' Pred:{} '.format(int(y_pred[idx[img_num]][0])), fontsize=20)
+                if j == 0:
+                    _ = ax.imshow(im_orig)
+                else:
+                    _ = ax.imshow(result_pp)
+                _ = fig.add_subplot(ax)
+        fig.show()
 
 
 def load_model(describe_model_nn, device, model_name):
